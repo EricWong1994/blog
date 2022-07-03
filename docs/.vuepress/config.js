c5ce1063f8c631cfcebff8d055f676d5366c3a46
搬砖达人小王git-flow <461864051@qq.com>
2022-07-03 09:38:25 +0800

    change project name

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	title: '吃核桃不吐皮儿',
+	description: '个人网站',
+	themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: '前端基础', link: '/guide/' },
+			{
+				text: 'Github',
+				link: 'https://github.com/EricWong1994/personal-website',
+			},
+		],
+	},
+};
